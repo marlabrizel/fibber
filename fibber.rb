81453ed189ec8453b0d7f7460118faa21c195698
Marla Brizel <marlabrizel@gmail.com>
2015-05-22 17:19:24 -0600

    Wrap fibonnaci in a function, complete challenge 1

--- fibber.rb
+++ fibber.rb
@@ -1,3 +1,11 @@
 quantity = 15
 
-(1..quantity-2).inject([0, 1]) { |fibs, n| fibs << (fibs[-1] + fibs[-2]) }
+def fibonacci(quantity)
+  (1..quantity-2).inject([0, 1]) { |fibs, n| fibs << (fibs[-1] + fibs[-2]) }
+end
+
+# Challenge 1
+
+fibonacci(quantity).each_slice(5) do |a,b,c,d,e|
+  p [a,b,c,d,e].join(", ")
+end
